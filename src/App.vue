de2cefcd0f3aec7fa1b4e478c15c3edf016dc4e3
20Federico <fefe.molino@gmail.com>
2021-11-17 16:36:52 +0100

    Milestone 1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label for="search">Cerca un film o una serie tv</label>
+    <input @keyup.enter="searchContent"  id="search" type="text" v-model="keyWord">
+    <button @click="searchContent" type="buttom">cerca</button>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{movie.title}}
+        <ul>
+          <li>{{movie.original_title}}</li>
+          <li>{{movie.original_language}}</li>
+          <li>{{movie.vote_average}}</li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      apiKey: "74546dbedf2125e52d6aff0b896163c3",
+      apiUrl: "https://api.themoviedb.org/3",
+      movies: [],
+      series: [],
+      keyWord: ""
+    }
+  },
+  methods: {
+    apiCall(categoryToSearch) {
+      axios.get(this.apiUrl + "/search/" + categoryToSearch, {
+      params: {
+        api_key: this.apiKey,
+        query: this.keyWord,
+        language: "it"
+      }
+    }).then(resp => {
+      if ( categoryToSearch === 'movie') {
+        this.movies = resp.data.results
+      } else if (categoryToSearch === 'tv') {
+        this.series = resp.data.results
+      }
+
+    });
+    },
+    searchContent() {
+      this.apiCall('movie')
+      this.keyWord = ""
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

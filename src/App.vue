80cb8e29fd3668d72fa0e70b32b1a71e22d4696d
20Federico <fefe.molino@gmail.com>
2021-11-18 12:32:26 +0100

    updated languageFlag function

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,9 @@
     <ul v-for="(category, i) in categories" :key="i">
     <h3 v-if="category.length > 0">{{i.toUpperCase()}}</h3>
       <li v-for="element in category" :key="element.id">
-        <span v-if="i === 'movies'">{{element.title}}</span>
-        <span v-if="i === 'series'">{{element.name}}</span>
+        <span>{{ i === 'movies' ? element.title : element.name}}</span>
         <ul>
-          <li v-if="i === 'movies'">{{element.original_title}}</li>
-          <li v-if="i === 'series'">{{element.original_name}}</li>
+          <li>{{i === 'movies' ? element.original_title : element.original_name}}</li>
           <li><img style="width: 20px" :src="languageFlag(element.original_language)" :alt="element.original_language"></li>
           <li>{{element.vote_average}}</li>
         </ul>
@@ -60,33 +58,19 @@ export default {
       // this.keyWord = ""
     },
     languageFlag(lang) {
-      switch (lang) {
-        case "en":
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
-        break;
-
-        case "it":
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/255px-Flag_of_Italy.svg.png"
-        break;
-
-        case "fr":
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png"
-        break;
-
-        case "es":
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2560px-Flag_of_Spain.svg.png"
-        break;
-
-        case "de":
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png"
-        break;
-        
-        default: 
-        this.langFlagSrc = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Missing_flag.png"
-        break;
-        
+      if (lang === "en") {
+        return "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
+      } else if (lang === "it") {
+        return "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/255px-Flag_of_Italy.svg.png"
+      } else if (lang === "fr") {
+        return "https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png"
+      } else if (lang === "es") {
+        return "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2560px-Flag_of_Spain.svg.png"
+      } else if (lang === "de") {
+        return "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png"
+      } else {
+        return "https://upload.wikimedia.org/wikipedia/commons/2/2f/Missing_flag.png"
       }
-      return this.langFlagSrc
     }
   }
 }
